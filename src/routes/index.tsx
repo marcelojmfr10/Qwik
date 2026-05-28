5b806b17a5e5e4c757891ae7619dfb5e0ed15a73
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-05-28 11:38:49 -0600

    Estructura de proyecto y tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <span class="text-5xl">hola mundo</span>
     </>
   );
 });
---
+++ src/routes/layout.tsx
@@ -0,0 +1,16 @@
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+
+import styles from "./styles.css?inline";
+import Navbar from "~/components/shared/header/navbar";
+
+export default component$(() => {
+  useStyles$(styles);
+  return (
+    <>
+      <Navbar />
+      <main class="flex flex-col items-center justify-center">
+        <Slot />
+      </main>
+    </>
+  );
+});
