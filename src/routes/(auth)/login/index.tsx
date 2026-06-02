4fd49b56257806c0a27e99c032510d79df3800b6
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-06-02 12:35:01 -0600

    Formulario y validaciones tradicionales

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,19 +1,67 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  $,
+  component$,
+  useComputed$,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 
 import styles from "./login.css?inline";
 
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const formState = useStore({
+    email: "",
+    password: "",
+    formPosted: false,
+  });
+
+  const emailError = useComputed$(() => {
+    if (formState.email.includes("@")) return "";
+
+    return "not-valid";
+  });
+
+  const passwordError = useComputed$(() => {
+    if (formState.password.length >= 6) return "";
+
+    return "not-valid";
+  });
+
+  const isFormValid = useComputed$(() => {
+    if (emailError.value === "not-valid" || passwordError.value === "not-valid")
+      return false;
+
+    return true;
+  });
+
+  const onSubmit = $(() => {
+    formState.formPosted = true;
+    const { email, password } = formState;
+
+    console.log({ isFormValid: isFormValid.value });
+    console.log({ email, password });
+  });
+
   return (
-    <form class="login-form">
+    <form onSubmit$={onSubmit} class="login-form" preventdefault:submit>
       <div class="relative">
-        <input name="email" type="text" placeholder="Email address" />
+        <input
+          value={formState.email}
+          onInput$={(event, target) => (formState.email = target.value)}
+          class={formState.formPosted ? emailError.value : ""}
+          name="email"
+          type="text"
+          placeholder="Email address"
+        />
         <label for="email">Email Address</label>
       </div>
       <div class="relative">
         <input
-          id="password"
+          value={formState.password}
+          onInput$={(event, target) => (formState.password = target.value)}
+          class={formState.formPosted ? passwordError.value : ""}
           name="password"
           type="password"
           placeholder="Password"
@@ -21,12 +69,10 @@ export default component$(() => {
         <label for="password">Password</label>
       </div>
       <div class="relative">
-        <button>Ingresar</button>
+        <button type="submit">Ingresar</button>
       </div>
 
-      {/* <code>
-                { JSON.stringify( formState, undefined , 2 ) }
-            </code> */}
+      <code>{JSON.stringify(formState, undefined, 2)}</code>
     </form>
   );
 });
