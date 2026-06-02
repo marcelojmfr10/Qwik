619ead00874900e372378f3b2e45488213ba651d
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-06-02 11:08:22 -0600

    Múltiple layouts y rutas

---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <>
+      <h3>admin dashboard</h3>
+      <p>esta ruta debe de ser privada</p>
+    </>
+  );
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,18 +1,9 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 
 import styles from "./styles.css?inline";
-import Navbar from "~/components/shared/navbar/navbar";
-import { PokemonProvider } from "~/context";
 
 export default component$(() => {
   useStyles$(styles); // manera global
 
-  return (
-    <PokemonProvider>
-      <Navbar />
-      <main class="flex flex-col items-center justify-center">
-        <Slot />
-      </main>
-    </PokemonProvider>
-  );
+  return <Slot />;
 });
